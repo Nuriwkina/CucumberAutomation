3a2b33ab58fa9d3f50c4541f600a0d9528047a10
Nuriwkina <abnur94@gmail.com>
2023-03-15 14:56:54 -0500

    initial commit

---
+++ src/test/java/pages/Phone.java
@@ -0,0 +1,9 @@
+package pages;
+
+import utilites.Driver;
+
+public class Phone {
+    public static void main(String[] args) {
+        Driver.getDriver().get("https://google.com");
+    }
+}
---
+++ src/test/java/utilites/Driver.java
@@ -0,0 +1,62 @@
+package utilites;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
+
+public class Driver {
+    private static WebDriver driver;
+    private static String browser=Config.getValue("browser");
+    ////one to launch the browser
+
+    public static WebDriver getDriver() {
+        if (driver == null) {/// is it the fist time this line is getting triggered.
+            if(browser.equalsIgnoreCase("chrome")) {
+
+                ChromeOptions co = new ChromeOptions();
+                co.addArguments("--remote-allow-origins=*");
+                driver = new ChromeDriver(co);
+                driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+                driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+                driver.manage().window().maximize();
+                return driver;
+            }else if (browser.equalsIgnoreCase("firefox")){
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+                driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+                driver.manage().window().maximize();
+                return driver;
+            }
+            else{
+                System.out.println("Invalid Browser Type. Launching Default Browser");
+
+                driver = new ChromeDriver();
+                driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+                driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+                driver.manage().window().maximize();
+                return driver;
+            }
+
+        }else{
+            return driver;
+
+            ////if the driver is null
+        }
+    }
+    ////the second to quit the browser
+    public static void quitBrowser(){
+        if(driver != null){
+            driver.quit();
+            driver=null;
+        }
+    }
+///one for checking the browser
+    // the second for the driver itself(when checking if it is null or not)
+}
+
+
